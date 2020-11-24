ddff5507d414e8516b73f6d1d3d38e99ccc3c054
nkinani <nawfel.kinani@onera.fr>
2020-11-24 12:08:01 +0100

    [dev] - update paths & organisation - update solution

--- lib/Apollon-feature-IxxatCAN/Properties/AssemblyInfo.cs
+++ lib/Apollon-feature-IxxatCAN/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("APOLLON-feature-CAN")]
+[assembly: AssemblyTitle("Apollon-feature-IxxatCAN")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("APOLLON-feature-IxxatCAN")]
+[assembly: AssemblyProduct("Apollon-feature-IxxatCAN")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,10 +17,10 @@ using System.Runtime.InteropServices;
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
 // COM, affectez la valeur true à l'attribut ComVisible sur ce type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("e099e34d-8964-462f-9a70-d9f6a29c93fa")]
+[assembly: Guid("56df8172-8986-4835-9977-a4aad696e450")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
